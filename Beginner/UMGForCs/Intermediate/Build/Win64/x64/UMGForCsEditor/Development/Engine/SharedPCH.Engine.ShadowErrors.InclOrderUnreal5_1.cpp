921a2252014aa2115e010d0257ecea07e6a8247f
jcoder58 <4xcoder@gmail.com>
2024-07-23 15:02:41 -0400

    Delete Beginner/UMGForCs/Intermediate directory

--- Beginner/UMGForCs/Intermediate/Build/Win64/x64/UMGForCsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.33.31629
-#include "C:/UnrealEngine/CSharpForUnrealEngine5/CsTutorials/Beginner/UMGForCs/Intermediate/Build/Win64/x64/UMGForCsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
